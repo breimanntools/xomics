6ceb492c76039550e5cca64fca9063f6160933cd
stephanbreimann <stephanbreimann@yahoo.de>
2023-10-11 23:19:13 +0200

    Publish version 0.2.0 to solve conflicts

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,6 @@ import warnings
 
 sys.path.append(os.path.abspath('.'))
 
-
 # Create notebooks rst and table rst first
 from create_tables_doc import generate_table_rst
 from create_notebooks_docs import export_notebooks_to_rst
